80b87ef64854b40a9b2e2ab699f8471940680adc
kchendil <mkchendil@gmail.com>
2013-05-31 15:35:17 -0400

    updated layout and images.
    its better now.

--- gen/com/param/android/thalaivargal/R.java
+++ gen/com/param/android/thalaivargal/R.java
@@ -13,34 +13,31 @@ public final class R {
     public static final class drawable {
         public static final int anna=0x7f020000;
         public static final int barathi=0x7f020001;
-        public static final int bharathi=0x7f020002;
-        public static final int kamaraj=0x7f020003;
-        public static final int kamarajar=0x7f020004;
-        public static final int periyar=0x7f020005;
-        public static final int screenshot1=0x7f020006;
-        public static final int screenshot2=0x7f020007;
-        public static final int t=0x7f020008;
-        public static final int vavusi=0x7f020009;
+        public static final int kamarajar=0x7f020002;
+        public static final int periyar=0x7f020003;
+        public static final int t=0x7f020004;
+        public static final int vavusi=0x7f020005;
     }
     public static final class id {
-        public static final int anna_back=0x7f070001;
-        public static final int anna_content=0x7f070000;
-        public static final int anna_restart=0x7f070002;
-        public static final int barathi_back=0x7f070004;
-        public static final int barathi_content=0x7f070003;
-        public static final int barathi_next=0x7f070005;
-        public static final int kamarajar_back=0x7f070007;
-        public static final int kamarajar_content=0x7f070006;
-        public static final int kamarajar_next=0x7f070008;
-        public static final int main_layout=0x7f070009;
-        public static final int main_start=0x7f07000a;
-        public static final int menu_settings=0x7f070011;
-        public static final int periyar_back=0x7f07000c;
-        public static final int periyar_content=0x7f07000b;
-        public static final int periyar_next=0x7f07000d;
-        public static final int vavusi_back=0x7f07000f;
-        public static final int vavusi_content=0x7f07000e;
-        public static final int vavusi_next=0x7f070010;
+        public static final int LinearLayout01=0x7f070000;
+        public static final int anna_back=0x7f070002;
+        public static final int anna_content=0x7f070001;
+        public static final int anna_restart=0x7f070003;
+        public static final int barathi_back=0x7f070005;
+        public static final int barathi_content=0x7f070004;
+        public static final int barathi_next=0x7f070006;
+        public static final int credits=0x7f070012;
+        public static final int kamarajar_back=0x7f070008;
+        public static final int kamarajar_next=0x7f070009;
+        public static final int main_layout=0x7f07000a;
+        public static final int main_start=0x7f07000b;
+        public static final int periyar_back=0x7f07000e;
+        public static final int periyar_content=0x7f07000d;
+        public static final int periyar_next=0x7f07000f;
+        public static final int textView1=0x7f07000c;
+        public static final int vavusi_back=0x7f070010;
+        public static final int vavusi_content=0x7f070007;
+        public static final int vavusi_next=0x7f070011;
     }
     public static final class layout {
         public static final int activity_anna=0x7f030000;
@@ -55,9 +52,11 @@ public final class R {
     }
     public static final class string {
         public static final int anna=0x7f040007;
+        public static final int app_desc=0x7f04000d;
         public static final int app_name=0x7f040000;
         public static final int back=0x7f040009;
         public static final int barathi=0x7f040005;
+        public static final int credits=0x7f04000c;
         public static final int kamarajar=0x7f040004;
         public static final int main=0x7f040002;
         public static final int menu_settings=0x7f040001;
